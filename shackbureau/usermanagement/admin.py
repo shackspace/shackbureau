777d8fb671eb5571163068117a25b53db9825ea7
Andreas Madsack <andreas@madflex.de>
2015-07-18 15:18:29 +0200

    added admin for Member in usermanagement app

--- shackbureau/usermanagement/admin.py
+++ shackbureau/usermanagement/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Member
 
-# Register your models here.
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("member_id", "name", "surname")
+    list_filter = ("member_id", )
