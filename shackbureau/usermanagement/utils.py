efef168e66a5e7b6b9261a2c4550cb73b0670dd8
Andreas Madsack <andreas@madflex.de>
2015-07-18 20:15:08 +0200

    get_or_create instead of create

--- shackbureau/usermanagement/utils.py
+++ shackbureau/usermanagement/utils.py
@@ -64,7 +64,9 @@ def import_old_shit(filename):
 
             print('#' * 40)
             print('Saving {member_id} (BIC: {bic})'.format(**member_data))
-            Member.objects.create(**member_data)
+            member_id = member_data.pop('member_id')
+            Member.objects.get_or_create(member_id=member_id,
+                                         defaults=**member_data)
 
 
 bics = {}
