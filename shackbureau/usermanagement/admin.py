6a3e9089ba86bb7348d10b886d7bf338791a1458
Andreas Madsack <andreas@madflex.de>
2015-07-18 16:03:30 +0200

    use form for member admin

--- shackbureau/usermanagement/admin.py
+++ shackbureau/usermanagement/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Member
+from .forms import MemberForm
 
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ("member_id", "name", "surname")
     list_filter = ("member_id", )
+    readonly_fields = ('member_id', )
+    form = MemberForm
---
+++ shackbureau/usermanagement/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from localflavor.de.forms import DEZipCodeField
+from .models import Member
+
+
+class MemberForm(forms.ModelForm):
+    zip_code = DEZipCodeField()
+    iban_zip_code = DEZipCodeField(label='IBAN zip code')
+
+    class Meta(object):
+        model = Member
+        fields = [
+            'member_id',
+            'join_date',
+            'name',
+            'nickname',
+            'surname',
+            'address1', 'address2',
+            'zip_code', 'city',
+            'country',
+            'is_underaged',
+            'email',
+            'date_of_birth',
+            'membership_type',
+            'membership_fee_monthly',
+            'membership_fee_interval',
+            'iban_fullname',
+            'iban_address',
+            'iban_zip_code',
+            'iban_city',
+            'iban_country',
+            'bic',
+            'iban',
+        ]
