423295919a99246555c9195129c55eaa62bc7f48
Andreas Madsack <andreas@madflex.de>
2015-07-26 12:19:20 +0200

    tests for konto_to_iban

--- shackbureau/tests/test_usermanagement.py
+++ shackbureau/tests/test_usermanagement.py
@@ -48,3 +48,19 @@ class TestMember:
 
     def test_two_member_ids_set(self, member_fixture_transfer, member_fixture_sepa):
         assert member_fixture_sepa.member_id != member_fixture_transfer.member_id
+
+
+class TestSepa:
+
+    @pytest.fixture(params=[
+        ('64090100', '1234567', 'VBRTDE6RXXX', 'DE80640901000001234567'),
+        (64090100, 1234567, 'VBRTDE6RXXX', 'DE80640901000001234567'),
+        (11111111, 1111111111, False, 'DE63111111111111111111'),
+    ])
+    def iban_fixture(self, request):
+        return dict(zip(('blz', 'kto', 'bic', 'iban'), request.param))
+
+    def test_iban_creation(self, iban_fixture):
+        from usermanagement.utils import konto_to_iban
+        assert konto_to_iban(iban_fixture.get('blz'),
+                             iban_fixture.get('kto')) == iban_fixture.get('iban')
