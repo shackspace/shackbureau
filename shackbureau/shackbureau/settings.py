d4357b0c609ca61ef41237d70b8f3b1680cb70ef
Andreas Madsack <andreas@madflex.de>
2015-07-26 13:30:33 +0200

    add test settings for wercker

--- shackbureau/shackbureau/settings.py
+++ shackbureau/shackbureau/settings.py
@@ -91,6 +91,19 @@ DATABASES = {
     }
 }
 
+if os.environ.get('TEST_ON_PLATFORM', '').lower() == 'wercker':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'HOST': os.environ.get('WERCKER_POSTGRESQL_HOST', '127.0.01'),
+            'NAME': os.environ.get('WERCKER_POSTGRESQL_DATABASE', 'test'),
+            'TEST_NAME': os.environ.get('WERCKER_POSTGRESQL_DATABASE', 'test'),
+            'USER': os.environ.get('WERCKER_POSTGRESQL_USERNAME', 'postgres'),
+            'PASSWORD': os.environ.get('WERCKER_POSTGRESQL_PASSWORD', ''),
+            'HOST': os.environ.get('WERCKER_POSTGRESQL_HOST', '127.0.01'),
+            'PORT': os.environ.get('WERCKER_POSTGRESQL_PORT', '5432'),
+        }
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
