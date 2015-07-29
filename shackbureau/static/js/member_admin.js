4f83a33394956c369e5a6556d80c55ad2da8e6bf
Andreas Madsack <andreas@madflex.de>
2015-07-29 13:57:13 +0200

    show/hide iban fields in admin member form based on payment_type.
    there must be a better way. but works for now.
    
    fixes #2

--- shackbureau/static/js/member_admin.js
+++ shackbureau/static/js/member_admin.js
@@ -1,1 +1,42 @@
 // member_admin.js
+var $ = django.jQuery;
+
+$(function(){
+
+$('#id_payment_type').show(function(){
+  if(this.value==="SEPA") {
+    show_iban_fields();
+  } else {
+    hide_iban_fields();
+  }
+});
+
+$('#id_payment_type').change(function(){
+  if(this.value==="SEPA") {
+    show_iban_fields();
+  } else {
+    hide_iban_fields();
+  }
+});
+
+function show_iban_fields() {
+    $('.field-iban').show();
+    $('.field-bic').show();
+    $('.field-iban_fullname').show();
+    $('.field-iban_address').show();
+    $('.field-iban_zip_code').show();
+    $('.field-iban_city').show();
+    $('.field-iban_country').show();
+}
+
+function hide_iban_fields() {
+    $('.field-iban').hide();
+    $('.field-bic').hide();
+    $('.field-iban_fullname').hide();
+    $('.field-iban_address').hide();
+    $('.field-iban_zip_code').hide();
+    $('.field-iban_city').hide();
+    $('.field-iban_country').hide();
+}
+
+});
