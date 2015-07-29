52ebf896742002d09801472f7fdea4745b48ad5d
Andreas Madsack <andreas@madflex.de>
2015-07-29 17:12:36 +0200

    optimize the selector for iban fields

--- shackbureau/static/js/member_admin.js
+++ shackbureau/static/js/member_admin.js
@@ -5,38 +5,22 @@ $(function(){
 
 $('#id_payment_type').show(function(){
   if(this.value==="SEPA") {
-    show_iban_fields();
+    iban_fields().show();
   } else {
-    hide_iban_fields();
+    iban_fields().hide();
   }
 });
 
 $('#id_payment_type').change(function(){
   if(this.value==="SEPA") {
-    show_iban_fields();
+    iban_fields().show();
   } else {
-    hide_iban_fields();
+    iban_fields().hide();
   }
 });
 
-function show_iban_fields() {
-    $('.field-iban').show();
-    $('.field-bic').show();
-    $('.field-iban_fullname').show();
-    $('.field-iban_address').show();
-    $('.field-iban_zip_code').show();
-    $('.field-iban_city').show();
-    $('.field-iban_country').show();
-}
-
-function hide_iban_fields() {
-    $('.field-iban').hide();
-    $('.field-bic').hide();
-    $('.field-iban_fullname').hide();
-    $('.field-iban_address').hide();
-    $('.field-iban_zip_code').hide();
-    $('.field-iban_city').hide();
-    $('.field-iban_country').hide();
+function iban_fields() {
+    return $('.field-iban, .field-bic, .field-iban_fullname, .field-iban_address, .field-iban_zip_code, .field-iban_city, .field-iban_country');
 }
 
 });
