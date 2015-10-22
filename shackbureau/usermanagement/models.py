260c1825eda7537ca06e8aee4015b7abf3770dba
Andreas Madsack <andreas@madflex.de>
2015-10-22 19:55:40 +0200

    pep8

--- shackbureau/usermanagement/models.py
+++ shackbureau/usermanagement/models.py
@@ -98,9 +98,9 @@ class Member(models.Model):
     bic = BICField(null=True, blank=True, verbose_name="BIC")
 
     iban_issue_date = models.DateField(
-            null=True, blank=True,
-            verbose_name="IBAN Issue Date",
-            help_text="The issue date of the direct debit mandate")
+        null=True, blank=True,
+        verbose_name="IBAN Issue Date",
+        help_text="The issue date of the direct debit mandate")
 
     iban_fullname = models.CharField(
         null=True, blank=True,
