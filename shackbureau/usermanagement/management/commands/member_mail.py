0365c64681370d75c2263e8911be2cae073f5f8c
Andreas Madsack <andreas@madflex.de>
2015-10-22 21:21:53 +0200

    send mails to all members as management command.
    needs some love for the mail template

---
+++ shackbureau/usermanagement/management/commands/member_mail.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+from django.core.management import BaseCommand
+from django.template import Context
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    help = "Sends an email to all active members"
+
+    def handle(self, *args, **options):
+        from usermanagement.models import Member, MemberTrackingCode
+        for member in Member.objects.filter(is_active=True):
+            if not member.email:
+                continue
+
+            uuid = MemberTrackingCode.objects.create(member=member,
+                                                     created_by=User.objects.get(username="admin"))
+            context = {
+                'uuid': uuid,
+                'member': member,
+            }
+            content = get_template('active_member_mail.txt').render(Context(context))
+
+            print(member.email)
+
+            email = EmailMessage('Subject please',
+                                 content,
+                                 'vorstand@shackspace.de',
+                                 [member.email])
+            email.send()
