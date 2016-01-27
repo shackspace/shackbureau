409c9eaf76ea22f19cb0836b7c4a3d11549efc06
ttb <ttb@shackspace.de>
2016-01-27 22:07:46 +0100

    validation of sepa information in member-admin

--- shackbureau/usermanagement/forms.py
+++ shackbureau/usermanagement/forms.py
@@ -33,6 +33,26 @@ class MemberForm(forms.ModelForm):
             # readonly
             'created_by',
         ]
+    def clean(self):
+        cleaned_data = super(MemberForm, self).clean()
+        payment_type = cleaned_data.get("payment_type")
+
+        if payment_type == "SEPA":
+            sepa_fields = ('iban_issue_date',
+                           'iban_fullname',
+                           'iban_address',
+                           'iban_zip_code',
+                           'iban_city',
+                           'iban_country',
+                           'bic',
+                           'iban',)
+            sepa_msg = "must be set if payment type is SEPA"
+            for field in sepa_fields:
+                if not cleaned_data.get(field):
+                    self.add_error(field, sepa_msg)
+
+        return cleaned_data
+
 
 class MemberSpecialsForm(forms.ModelForm):
     class Meta(object):
@@ -61,3 +81,5 @@ class MemberSpecialsForm(forms.ModelForm):
         if is_keyholder and not ssh_public_key:
             msg = "A keyholder must have a ssh public key"
             self.add_error('ssh_public_key', msg)
+
+        return cleaned_data
