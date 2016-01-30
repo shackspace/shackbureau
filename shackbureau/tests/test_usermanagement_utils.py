6d00e2c36b27bcc4536e7a33d0963f6a464df3b8
ttb <ttb@shackspace.de>
2016-01-30 01:15:52 +0100

    fix test_usermanagement_utils

--- shackbureau/tests/test_usermanagement_utils.py
+++ shackbureau/tests/test_usermanagement_utils.py
@@ -10,7 +10,7 @@ class TestMemberUtils:
         from datetime import date
         assert last_day_of_month(date(2015, 1, 24)) == date(2015, 1, 31)
         assert last_day_of_month(date(2016, 2, 28)) == date(2016, 2, 29)  # leap year
-        assert last_day_of_month(date(2015, 2, 28)) == date(2016, 2, 28)  # not a leap year
+        assert last_day_of_month(date(2015, 2, 28)) == date(2015, 2, 28)  # not a leap year
         assert last_day_of_month(date(2014, 12, 3)) == date(2014, 12, 31)
         assert last_day_of_month(date(1994, 1, 1)) == date(1994, 1, 31)
         assert last_day_of_month(date(2023, 12, 24)) == date(2023, 12, 31)
