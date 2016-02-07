07bb07a53d54554a92dfec1bdc8f3afedb99cc1b
ttb <ttb@shackspace.de>
2016-02-07 02:36:00 +0100

    new mailman api

--- shackbureau/shackbureau/production_settings_template.py
+++ shackbureau/shackbureau/production_settings_template.py
@@ -1,11 +1,10 @@
-MAILMAN_MITGLIEDER_PW = None
-MAILMAN_MITGLIEDER_ANNOUNCE_PW = None
+MAILMAN_API_USER = None
+MAILMAN_API_PASSWORD = None
 CASHMASTER_MAILADDR = 'foo@shackspace.de'
 
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = ""
 EMAIL_PORT = 587
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = r""
 EMAIL_USE_TLS = True
-
--- shackbureau/usermanagement/utils.py
+++ shackbureau/usermanagement/utils.py
@@ -115,40 +115,19 @@ def blz_to_bic(blz):
 
 def add_to_mailman(mailaddr, mitgliederml=True):
     from django.conf import settings
-    if not settings.MAILMAN_MITGLIEDER_PW or not settings.MAILMAN_MITGLIEDER_ANNOUNCE_PW:
+    if not settings.MAILMAN_API_USER or not settings.MAILMAN_API_PASSWORD:
+        print("add_to_mailman not possible\nplease add MAILMAN_API_USER and MAILMAN_API_PASSWORD to production settings")
         return
 
-    mitglieder_url = 'https://lists.shackspace.de/mailman/admin/mitglieder'
-    mitglieder_subscribe_url = 'https://lists.shackspace.de/mailman/admin/mitglieder/members/add'
-    mitglieder_announce_url = 'https://lists.shackspace.de/mailman/admin/mitglieder-announce'
-    mitglieder_announce_subscribe_url = 'https://lists.shackspace.de/mailman/admin/mitglieder-announce/members/add'
-
-    s = requests.Session()
     if mitgliederml:
-        login_payload = {'adminpw': settings.MAILMAN_MITGLIEDER_PW,
-                         'admlogin': 'Let me in...'}
-        r = s.post(mitglieder_url, params=login_payload, verify=False)
-        assert r.status_code == 200
-        subscribe_payload = {'subscribe_or_invite': 0,
-                             'send_welcome_msg_to_this_batch': 1,
-                             'send_notifications_to_list_owner': 0,
-                             'subscribees': mailaddr,
-                             'invitation': '',
-                             'setmemberopts_btn': 'Submit Your Changes'}
-        r = s.post(mitglieder_subscribe_url, params=subscribe_payload, verify=False)
+        r = requests.post("https://shackspace.de/mlm-api/lists.shackspace.de/mitglieder/{}".format(mailaddr),
+                          auth=(settings.MAILMAN_API_USER, settings.MAILMAN_API_PASSWORD),
+                          verify=False)
         assert r.status_code == 200
 
-    login_payload = {'adminpw': settings.MAILMAN_MITGLIEDER_ANNOUNCE_PW,
-                     'admlogin': 'Let me in...'}
-    r = s.post(mitglieder_announce_url, params=login_payload, verify=False)
-    assert r.status_code == 200
-    subscribe_payload = {'subscribe_or_invite': 0,
-                         'send_welcome_msg_to_this_batch': 1,
-                         'send_notifications_to_list_owner': 0,
-                         'subscribees': mailaddr,
-                         'invitation': '',
-                         'setmemberopts_btn': 'Submit Your Changes'}
-    r = s.post(mitglieder_announce_subscribe_url, params=subscribe_payload, verify=False)
+    r = requests.post("https://shackspace.de/mlm-api/lists.shackspace.de/mitglieder-announce/{}".format(mailaddr),
+                      auth=(settings.MAILMAN_API_USER, settings.MAILMAN_API_PASSWORD),
+                      verify=False)
     assert r.status_code == 200
 
 
