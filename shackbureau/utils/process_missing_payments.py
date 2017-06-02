bab0423fda4e77cb905ab5686847e18811697097
Tobias Kunze <rixx@cutebit.de>
2017-06-02 10:33:24 +0200

    Implement fancy menu

--- shackbureau/utils/process_missing_payments.py
+++ shackbureau/utils/process_missing_payments.py
@@ -7,6 +7,24 @@ def print_help():
     print('Usage: print_missing_payments.py <path/to/export.csv> <path/to/mail_template>')
 
 
+def print_member(member):
+    flag = f' – ausgetreten am {member["leave_date"]}' if not member['is_active'] else ''
+    if member['nickname']:
+        print(f'{member["name"]} ({member["nickname"]}){flag}')
+    else:
+        print(f'{member["name"]} {flag}')
+    print(f'Schuldet {member["accumulated_balance"]} €, davon {member["balance"]} € von {member["year"]}')
+    print(f'Letzte Zahlungen: http://localhost:8000/admin/usermanagement/accounttransaction/?booking_type__exact=deposit&q={member["name"].replace(" ", "+")}')
+    print()
+
+
+def write_dict(data, fieldnames):
+    writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+    writer.writeheader()
+    for element in data:
+        writer.writerow(element)
+
+
 def main():
     template_path = sys.argv[-1]
     csv_path = sys.argv[-2]
@@ -15,7 +33,24 @@ def main():
         with open(template_path, 'r') as template_file:
             template_text = template_file.read()
         with open(csv_path) as csv_file:
-            members = csv.DictReader(csv_file)
+            reader = csv.DictReader(csv_file)
+            members = [m for m in reader]
+            for member in members:
+                print_member(member)
+                action = input('(m)ail, (s)kip, (d)elete from list, (w)rite list and quit, (q)uit: ')
+
+                if action == 'q':
+                    return
+                elif action == 'w':
+                    write_dict(members, reader.fieldnames)
+                    return
+                elif action == 'd':
+                    del(member)
+                elif action == 's':
+                    continue
+                elif action == 'm':
+                    print(template_text.replace('{{ member_id }}', member['member_id']).replace('{{ amount }}', member['accumulated_balance']).replace('{{ email }}', member['email']))
+
     except Exception as e:
         print(e)
         print_help()
